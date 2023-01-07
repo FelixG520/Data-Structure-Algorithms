c23b4794cb20145fd1577909377ab1578317881c
Felix_G <g13346328091@163.com>
2023-01-07 14:53:46 +0800

    顺序表

--- C语言描述/2.顺序表/2.顺序表.c
+++ C语言描述/2.顺序表/2.顺序表.c
@@ -1,14 +1,108 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "Seqlist.h"
-
-void SeaListInit(SL ps)//初始化
+//打印顺序表中内容
+void SeqListPrint(SL* ps)
 {
-	ps.a = NULL;
-	ps.size = ps.capacity = 0;
+	for (int i = 0; i < ps->size; i++)
+	{
+		printf("%d ", ps->a[i]);
+	}
+	printf("\n");
+}
+//销毁空间
+void SeqListDestory(SL* ps)
+{
+	free(ps->a);
+	ps->a = NULL;
+	ps->capacity = ps->size = 0;
+}
+void SeaListInit(SL* ps)//初始化
+{
+	ps->a = NULL;
+	ps->size = 0;
+	ps->capacity = 0;
 }
 
-void SeaListPushBack(SL* ps, SLDataType x);//尾插
-//也可写成：void SeaListPushBack(struct Sqqlist* ps, SLDataType x);
-void SeaListPopBack(SL* ps);//尾删
-void SeaListPushFront(SL* ps, SLDataType x);//头插
-void SeaListPushBack(SL* ps);//头删
+//扩容
+void SeqlistCheckCapacity(SL* ps)
+{
+	if (ps->size == ps->capacity)
+	{
+		//如果是0，就给四个空间，如果capacity满了就扩大n倍，随便自己扩大
+		int newcapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
+		SLDataType* tmp = (SLDataType*)realloc(ps->a, newcapacity * sizeof(SLDataType));
+		{
+			//如果空间开辟失败
+			if (tmp == NULL)
+			{
+				printf("realloc fail\n");
+				exit(-1);
+			}
+			//空间开辟成功
+			ps->a = tmp;
+			ps->capacity = newcapacity;
+		}
+	}
+}
+
+//********************************************************************
+void SeqListPushBack(SL* ps, SLDataType x)//尾插
+{
+	SeqlistCheckCapacity(ps);
+	//空间足够的处理
+	ps->a[ps->size] = x;
+	ps->size++;
+}
+
+
+//*****************************************************************************
+void SeqListPopBack(SL* ps)//尾删
+{
+	//温柔处理方式
+	if (ps->size > 0)
+	{
+		//ps->a[ps->size - 1] = 0;//最后一个数据置0,这句可以不要，因为就算置0，开辟的内存还在
+		ps->size--;
+	}
+}
+
+//#include <assert.h>
+//void SeqListPopBack(SL* ps)//尾删
+//{
+//	//暴力处理方式
+//	assert(ps->size > 0);
+//	ps->size--;
+//}
+
+
+
+//**************************************************************************************
+void SeqListPushFront(SL* ps, SLDataType x)//头插
+{
+	SeqlistCheckCapacity(ps);
+	//把所有元素往后平移一个，把要插入的元素放到前面
+	int end = ps->size - 1;
+	while (end >= 0)
+	{
+		ps->a[end + 1] = ps->a[end];
+		end--;
+	}
+	ps->a[0] = x;
+	ps->size++;
+}
+
+
+//*******************************************************************************
+#include <assert.h>
+void SeqListPopFront(SL* ps)//头删
+{
+	assert(ps->size > 0);
+	//挪动数据
+	int start = 1;
+	while (start<ps->size)
+	{
+		ps->a[start - 1] = ps->a[start];//从下标为1的元素开始
+		start++;
+	}
+	ps->size--;
+}
--- C语言描述/2.顺序表/Seqlist.h
+++ C语言描述/2.顺序表/Seqlist.h
@@ -1,11 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include <stdlib.h>
-#pragma once
-#define N 1000
-
-typedef int SLDataType;//类型定义，int float double...
-
 /*
 顺序表概念及结构
 顺序表是用一段物理地址连续的存储单元依次存储数据元素的线性结构，一般情况下采用数组存储。
@@ -14,17 +6,24 @@ typedef int SLDataType;//类型定义，int float double...
 顺序表就是数组，但是在数组的基础上，它还要求数据是连续存储的(从头开始存)，不能跳跃间隔
 */
 
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#pragma once
+#define N 1000
+typedef int SLDataType;//类型重定义，int float double...
+
+
+
 //静态顺序表
 //静态的特点：如果满了就不让插入    缺点：给多少的合适呢？这个很难确定。N给小了不够用，N给大了浪费
-typedef struct Seqlist
-{
-	SLDataType a[N];//数组
-	int size;//表示数组中存储了多少个数据
-}SL;
+//typedef struct Seqlist
+//{
+//	SLDataType  a[N];//数组
+//	int size;//表示数组中存储了多少个数据
+//}SL;
 
-//接口函数 -- 命名风格是跟着STL走的，方便后续学习STL
-void SeaListInit(SL* ps);//初始化
-
+//**********************************************************************************************************
 
 
 //动态顺序表
@@ -35,11 +34,22 @@ typedef struct Seqlist
 	int capacity;//数组实际能存数据的空间容量是多大
 }SL;
 
+
+
+
+void SeqListPrint(SL* ps);
+void SeqListDestory(SL* ps);
+void SeqlistCheckCapacity(SL* ps);
+
+
 //接口函数 -- 命名风格是跟着STL走的，方便后续学习STL
-void SeaListPushBack(SL* ps, SLDataType x);//尾插
-//也可写成：void SeaListPushBack(struct Sqqlist* ps, SLDataType x);
-void SeaListPopBack(SL* ps);//尾删
-void SeaListPushFront(SL* ps, SLDataType x);//头插
-void SeaListPushBack(SL* ps);//头删
-
-
+void SeaListInit(SL* ps);//初始化
+void SeqListPushBack(SL* ps, SLDataType x);//尾插
+//也可写成：void SeqListPushBack(struct Sqqlist* ps, SLDataType x);
+void SeqListPopBack(SL* ps);//尾删
+void SeqListPushFront(SL* ps, SLDataType x);//头插
+void SeqListPopFront(SL* ps);//头删
+// ...
+int SeqListFind(SL* ps, SLDataType x);//找到了返回x位置下标，没有找到返回-1
+void SeqListInsert(SL* ps, int pos,SLDataType x);//在指定下标位置插入
+void SeqListErase(SL* ps, int pos, SLDataType x);//删除任意位置元素
