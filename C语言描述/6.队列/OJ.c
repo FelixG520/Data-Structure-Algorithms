057fae13942f2ee6875f65bbe3758d3dc2971d04
Felix_G <g13346328091@163.com>
2023-01-16 14:09:08 +0800

    队列OJ题

--- C语言描述/6.队列/OJ.c
+++ C语言描述/6.队列/OJ.c
@@ -1,2 +1,68 @@
 #include "Queen.h"
+//用队列实现栈
+typedef struct
+{
+	Queue q1;
+	Queue q2;
+}MyStack;
+
+MyStack* myStackCreate()
+{
+	MyStack* st =(MyStack*) malloc(sizeof(MyStack));
+	if (st == NULL)
+	{
+		printf("Malloc fail!\n");
+	}
+	QueueInit(&(st->q1));
+	QueueInit(&st->q2);
+	return st;
+}
+
+void myStackPush(MyStack* obj, int x)
+{
+	if (!QueueEmpty(&obj->q1))
+		QueuePush(&obj->q1, x);
+	else
+		QueuePush(&obj->q2, x);
+}
+int myStackPop(MyStack* obj)
+{
+	Queue* emptyQ = &obj->q1;
+	Queue* nonemptyQ = &obj->q2;
+	//默认q1为空，q2不为空
+	if (!QueueEmpty(&obj->q1))
+	{
+		emptyQ = &obj->q2;
+		nonemptyQ = &obj->q1;
+	}
+	while (QueueSize(nonemptyQ) > 1)//只剩最后一个元素时，删掉
+	{
+		//取出放到空队列，并把非空那边删掉
+		QueuePush(emptyQ, QueueFront(nonemptyQ));
+		QueuePop(nonemptyQ);
+	}
+	int top = QueueFront(nonemptyQ);
+	QueuePop(nonemptyQ);
+	return top;
+}
+
+int myStackTop(MyStack* obj)
+{
+	if (!QueueEmpty(&obj->q1))
+		return QueueBack(&obj->q1);
+	else
+		return QueueBack(&obj->q2);
+}
+bool myStackEmpty(MyStack* obj)
+{
+	return QueueEmpty(&obj->q1) && QueueEmpty(&obj->q2);
+}
+
+void myStakFree(MyStack* obj)
+{
+	QueueDestroy(&obj->q1);
+	QueueDestroy(&obj->q2);
+	free(obj);
+}
+
 
