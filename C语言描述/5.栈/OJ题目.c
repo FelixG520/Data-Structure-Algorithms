2790e67fdb9117e4e124ebf912c450ec09c7d5dd
Felix_G <g13346328091@163.com>
2023-01-16 14:09:22 +0800

    栈OJ题

--- C语言描述/5.栈/OJ题目.c
+++ C语言描述/5.栈/OJ题目.c
@@ -44,3 +44,64 @@ bool isValid(char* s)
 	STackDestory(&st);
 	return true;
 }
+
+
+//用栈实现队列
+typedef struct
+{
+	ST pushST;
+	ST popST;
+}MyQueue;
+
+MyQueue* myQueueCreate()
+{
+	MyQueue* q = (MyQueue*)malloc(sizeof(MyQueue));
+	StackInit(&q->pushST);
+	StackInit(&q->popST);
+	return q;
+}
+
+void myQueuePush(MyQueue* obj, int x)
+{
+	StackPush(&obj->pushST, x);
+}
+
+int myQueuePop(MyQueue* obj)
+{
+	if (StackEmpty(&obj->popST))//pop栈没有数据，就要从push栈里倒过来
+	{
+		while (!StackEmpty(&obj->pushST))
+		{
+			StackPush(&obj->popST, StackTop(&obj->pushST));
+			StackPop(&obj->pushST);
+		}
+	}
+	int front = StackTop(&obj->popST);
+	StackPop(&obj->popST);
+	return front;
+}
+
+int myQueuePeek(MyQueue* obj)
+{
+	if (StackEmpty(&obj->popST))//pop栈没有数据，就要从push栈里倒过来
+	{
+		while (!StackEmpty(&obj->pushST))
+		{
+			StackPush(&obj->popST, StackTop(&obj->pushST));
+			StackPop(&obj->pushST);
+		}
+	}
+	return StackTop(&obj->popST);
+}
+
+bool myQueueEmpty(MyQueue* obj)
+{
+	return StackEmpty(&obj->popST) && StackEmpty(&obj->pushST);
+}
+
+void myQueueFree(MyQueue* obj)
+{
+	QueueDestroy(&obj->popST);
+	QueueDestroy(&obj->pushST);
+	free(obj);
+}
